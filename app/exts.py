ca84c23790b84b3a8de218aa4526c78c999df8f6
Nicolas Berens <nicolas.berens@betrolltes-wohnen.de>
2022-03-20 20:53:47 +0100

    fix imports

--- app/exts.py
+++ app/exts.py
@@ -3,8 +3,8 @@
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
-from flask.ext.mail import Mail
+from flask_mail import Mail
 mail = Mail()
 
-from flask.ext.migrate import Migrate
+from flask_migrate import Migrate
 migrate = Migrate()
